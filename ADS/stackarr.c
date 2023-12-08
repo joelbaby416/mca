0783eee797f2771ad75652731c90ce70accb19fc
joelbaby416 <151810422+joelbaby416@users.noreply.github.com>
2023-12-08 06:27:05 +0000

    Add files via upload

---
+++ ADS/stackarr.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 100
+int main(){
+    int arr[MAX_SIZE];
+    int limit,top=-1,ele,i,ch;
+    printf("Enter the limit: ");
+    scanf("%d",&limit);
+    
+    do{
+      printf("\n1:Push\n2:Pop\n3:Display\n4:Exit\n");
+      scanf("%d",&ch);
+      
+      switch(ch){
+             case 1: //Push
+                     if(top>=limit-1){
+                       printf("Stack Overflow");
+                     }else{
+                          printf("Enter the Element\n");
+                          scanf("%d",&ele);
+                          top+=1;
+                          arr[top]=ele;
+                     }
+                     break;
+                     
+            case 2: //Pop
+                    if(top<=-1){
+                      printf("Stack underflow");
+                    }else{
+                      top--;
+                    }
+                    break;
+                      
+            case 3: //Display
+                    for(i=top;i>=0;i--){
+                        printf("%d\n",arr[i]);
+                    }
+                    break;
+                 
+           case 4: //Exit
+                   exit(1);
+           default: printf("Invalid Number");
+                    break;
+     }
+   }while(1);
+   return 0;
+}
+         
