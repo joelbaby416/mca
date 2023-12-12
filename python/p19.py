b9ef61fd56f15adbb89ce3e4c4d2fa7ba68f42c7
joelbaby416 <151810422+joelbaby416@users.noreply.github.com>
2023-12-12 10:24:13 +0000

    Add files via upload

--- python/p19.py
+++ python/p19.py
@@ -1,4 +1,9 @@
-original_list=[1,2,3,4,5,6,7,8,9,10]
-odd_numbers_list=[num for num in original_list if num%2!=0]
-print("original list:",original_list)
-print("List with odd numbers only:",odd_numbers_list)
+cn=raw_input("Enter comma-separated colour names")
+c=cn.split(',')
+c=[color.strip() for color in c]
+if len(c)>=2:
+			print("first color:",c[0])
+			print("last color:",c[-1])
+else:
+		print("please enter atleast two color names")
+		
