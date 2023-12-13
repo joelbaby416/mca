dbfca9ce28b3e2724e8f2ec769f1c0fa909d3581
joelbaby416 <151810422+joelbaby416@users.noreply.github.com>
2023-12-13 10:28:18 +0000

    Add files via upload

--- python/p14.py
+++ python/p14.py
@@ -1,5 +1,4 @@
-color_list_1=['red','green','blue','white','orange']
-color_list_2=['green','orange','black','white']
-result_colors=[color for color in color_list_1 if color not in color_list_2]
-
-print "colors from color_list_1 not contained in color_list_2:", result_colors
+a=3.14
+radius=int(input("enter the radius"))
+area=a*radius*radius
+print(area)
--- python/p15.py
+++ python/p15.py
@@ -1,10 +1,5 @@
-string1=raw_input("Enter the first string:")
-string2=raw_input("Enter the second string:")
-if len(string1)>0 and len(string2)>0:
-	new_string1=string2[0]+string1[1:]
-	new_string2=string1[0]+string2[1:]
-	result_string= new_string1+" "+new_string2
-	print"resulting string:",result_string
-else:
-	print("both strings must have atleast one character")
+import math as m
 
+radius=float(input("Please enter the radius of given circle"))
+area=m.pi*radius*radius
+print("The area of the given circle is  ",area)
