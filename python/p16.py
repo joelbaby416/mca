096f95adcd928a48d7222ea5fc27fbcf11024072
joelbaby416 <151810422+joelbaby416@users.noreply.github.com>
2023-12-13 10:40:52 +0000

    Add files via upload

--- python/p16.py
+++ python/p16.py
@@ -1,9 +1,6 @@
-my_dict={'one':1,'three':3,'five':5,'two':2,'four':4}
-sorted_dict_asc=dict(sorted(my_dict.items()))
-print"Disctionary in Ascending order by keys:",sorted_dict_asc
-sorted_dict_desc=dict(sorted(my_dict.items(),reverse=True))
-print"Disctionary in Descending order by keys:",sorted_dict_desc
-sorted_dict_asc_values=dict(sorted(my_dict.items(),key=lambda item:item[1]))
-print"Disctionary in Ascending order by values:",sorted_dict_asc_values
-sorted_dict_desc_values=dict(sorted(my_dict.items(),key=lambda item:item[1],reverse=True))
-print"Disctionary in Descending order by values:",sorted_dict_asc_values
+import math
+def area_of_the_circle(radius):
+	area=radius**2*math.pi
+	return area
+radius=float(input("please enter the radius:"))
+print("The area of the given circle is:",area_of_the_circle(radius))
